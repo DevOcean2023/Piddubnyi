a3f5a44fc1ca178947848584587e9588888fd3c5
Valentina <moskaliukvalentine@gmail.com>
2024-02-26 13:59:44 +0200

    add wp home-blog

--- wp-content/themes/child-theme/template-parts/flexible-content/blog-home.php
+++ wp-content/themes/child-theme/template-parts/flexible-content/blog-home.php
@@ -11,9 +11,9 @@
 			<?php dov_the( 'title', 'blog-home__head-title' ); ?>
 			<?php dov_the( 'button', 'top-btn btn' ); ?>
 		</div>
-
-		<?php $i = 1; ?>
+		<?php $i = 0; ?>
 		<?php while ( dov_loop( 'blog', '<div class="blog-home__wrapper">' ) ) : ?>
+			<?php ++$i; ?>
 			<a class="blog-home__item <?php echo ( $i == 1 ) ? 'blog-home__item_first' : ''; ?>" href="<?php the_permalink(); ?>">
 				<div class="blog-home__image">
 					<?php the_post_thumbnail( 'full' ); ?>
@@ -21,11 +21,11 @@
 				<div class="blog-home__content">
 					<h2 class="blog-home__title"><?php the_title(); ?></h2>
 					<p class="blog-home__excerpt">
-						<?php dov_the_excerpt( $args['is_first'] ? ' 160' : '100', false, true, '...' ); ?>
+						<?php echo $i; ?>
+						<?php dov_the_excerpt( ( $i == 1 ) ? ' 160' : '100', false, true, '...' ); ?>
 					</p>
 				</div>
 			</a>
-			<?php ++$i; ?>
 		<?php endwhile; ?>
 
 		<?php dov_the( 'button', 'bottom-btn btn' ); ?>
