aa0db15a790fd025cb406ad27da2f5d1c256f7c5
Valentina <moskaliukvalentine@gmail.com>
2024-02-26 09:48:45 +0200

    add wp trusted.php

--- wp-content/themes/child-theme/template-parts/flexible-content/trusted.php
+++ wp-content/themes/child-theme/template-parts/flexible-content/trusted.php
@@ -1,12 +1,14 @@
 <section class="trusted">
 	<div class="inner">
 		<?php dov_the( 'title', 'trusted__title' ); ?>
-		<?php while ( dov_loop( 'items', '<div class="marquee" data-gap="88">' ) ) : ?>
-			<div class="trusted__item">
-				<?php while ( dov_wrap( 'link' ) ) : ?>
-					<?php dov_the( 'image', '193x0' ); ?>
-				<?php endwhile; ?>
-			</div>
-		<?php endwhile; ?>
+		<div class="marquee" data-gap="88">
+			<?php while ( dov_loop( 'items', '<div class="trusted__items">' ) ) : ?>
+				<div class="trusted__item">
+					<?php while ( dov_wrap( 'link' ) ) : ?>
+						<?php dov_the( 'image', '193x0' ); ?>
+					<?php endwhile; ?>
+				</div>
+			<?php endwhile; ?>
+		</div>
 	</div>
 </section>
