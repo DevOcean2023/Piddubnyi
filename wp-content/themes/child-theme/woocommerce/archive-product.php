e9ad60df1d1e2f20bbb563aff8663790b9aa46fb
Goretskyi Oleg <goretsoleg@gmail.com>
2024-02-27 11:34:44 +0200

    - category banner test

--- wp-content/themes/child-theme/woocommerce/archive-product.php
+++ wp-content/themes/child-theme/woocommerce/archive-product.php
@@ -46,8 +46,21 @@ do_action( 'woocommerce_before_main_content' );
 
 			<div class="shop-page__products-holder">
 				<header class="woocommerce-products-header">
-					<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-						<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+					<?php if ( is_product_category( 'sale' ) ) : ?>
+						<div class="sale-banner">
+							<figure class="sale-logo-holder">
+								<img
+									src="<?php echo get_stylesheet_directory_uri(); ?>/assest/images/ico-sale-logo.svg"
+									alt="sale logo">
+							</figure>
+							<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+								<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+							<?php endif; ?>
+						</div>
+					<?php else : ?>
+						<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+							<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+						<?php endif; ?>
 					<?php endif; ?>
 
 					<?php
