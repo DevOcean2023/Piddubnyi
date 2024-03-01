0ae79f621c48730d3ff4de8d7813fa3d2002cb72
Goretskyi Oleg <goretsoleg@gmail.com>
2024-03-01 14:41:15 +0200

    - page load test

--- wp-content/themes/child-theme/header.php
+++ wp-content/themes/child-theme/header.php
@@ -20,7 +20,7 @@ echo wp_kses(
 echo '<head>';
 wp_head();
 echo '<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">';
-echo '<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">';
+//echo '<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">';
 echo '</head>';
 
 echo '<body class="' . esc_attr( implode( ' ', get_body_class() ) ) . '">';
