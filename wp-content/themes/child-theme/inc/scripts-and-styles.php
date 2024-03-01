bfb3e3f6e6bcbc73d8abd06944b9e3973bd53c07
Goretskyi Oleg <goretsoleg@gmail.com>
2024-03-01 16:52:46 +0200

    - page load test

--- wp-content/themes/child-theme/inc/scripts-and-styles.php
+++ wp-content/themes/child-theme/inc/scripts-and-styles.php
@@ -26,15 +26,13 @@ function theme_scripts() {
 			false
 		);
 	}
-	if ( is_woocommerce() ) {
-		wp_enqueue_script(
-			'dov',
-			get_stylesheet_directory_uri() . '/assets/js/dov.js',
-			array( 'jquery' ),
-			false,
-			false
-		);
-	}
+	wp_enqueue_script(
+		'dov',
+		get_stylesheet_directory_uri() . '/assets/js/dov.js',
+		array( 'jquery' ),
+		false,
+		false
+	);
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
