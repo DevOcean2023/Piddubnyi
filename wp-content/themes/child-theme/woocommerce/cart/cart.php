92fb80b2e09477b9cfa294e65807276067f7b79b
Goretskyi Oleg <goretsoleg@gmail.com>
2024-03-01 10:54:44 +0200

    - cart coupon field fixed

--- wp-content/themes/child-theme/woocommerce/cart/cart.php
+++ wp-content/themes/child-theme/woocommerce/cart/cart.php
@@ -153,35 +153,36 @@ do_action( 'woocommerce_before_cart' ); ?>
 		</tbody>
 	</table>
 	<?php do_action( 'woocommerce_after_cart_table' ); ?>
-</form>
 
-<?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
 
-<div class="cart-footer">
+	<?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
 
-	<?php if ( wc_coupons_enabled() ) { ?>
-		<div class="coupon">
-			<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label>
-			<input
-				type="text" name="coupon_code" class="input-text" id="coupon_code" value=""
-				placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>"/>
-			<button type="submit" class="button" name="apply_coupon"
-					value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-			<?php do_action( 'woocommerce_cart_coupon' ); ?>
+	<div class="cart-footer">
+
+		<?php if ( wc_coupons_enabled() ) : ?>
+			<div class="coupon">
+				<label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label>
+				<input
+					type="text" name="coupon_code" class="input-text" id="coupon_code" value=""
+					placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>"/>
+				<button type="submit" class="button" name="apply_coupon"
+						value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
+				<?php do_action( 'woocommerce_cart_coupon' ); ?>
+			</div>
+		<?php endif; ?>
+
+		<div class="cart-collaterals">
+			<?php
+			/**
+			 * Cart collaterals hook.
+			 *
+			 * @hooked woocommerce_cross_sell_display
+			 * @hooked woocommerce_cart_totals - 10
+			 */
+			do_action( 'woocommerce_cart_collaterals' );
+			?>
 		</div>
-	<?php } ?>
-
-	<div class="cart-collaterals">
-		<?php
-		/**
-		 * Cart collaterals hook.
-		 *
-		 * @hooked woocommerce_cross_sell_display
-		 * @hooked woocommerce_cart_totals - 10
-		 */
-		do_action( 'woocommerce_cart_collaterals' );
-		?>
 	</div>
-</div>
+</form>
 
 <?php do_action( 'woocommerce_after_cart' ); ?>
