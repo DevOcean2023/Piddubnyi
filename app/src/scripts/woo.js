e40fd293e8712bc4294ed2af69b87928d0407ef1
Valentina <moskaliukvalentine@gmail.com>
2025-04-11 13:35:12 +0300

    fix code

--- app/src/scripts/woo.js
+++ app/src/scripts/woo.js
@@ -33,36 +33,45 @@ function initCustomSelects() {
 
 ////filer-button-expand
 document.addEventListener("DOMContentLoaded", function () {
-	const liElements = document.querySelectorAll(".wpfFilterVerScroll li");
+	const observer = new MutationObserver(function (mutationsList, observer) {
+		if (document.querySelector(".shop-page")) {
+			observer.disconnect();
+			const liElements = document.querySelectorAll(".wpfFilterVerScroll li");
 
-	liElements.forEach(function (li) {
-		const ulElement = li.querySelector("ul");
+			liElements.forEach(function (li) {
+				const ulElement = li.querySelector("ul");
 
-		if (ulElement) {
-			const divElement = document.createElement("div");
-			divElement.className = "filer-button-expand";
-			li.insertBefore(divElement, li.querySelector('.wpfLiLabel'));
+				if (ulElement) {
+					const divElement = document.createElement("div");
+					divElement.className = "filer-button-expand";
+					li.insertBefore(divElement, li.querySelector('.wpfLiLabel'));
+				}
+			});
+
+			const expandButtons = document.querySelectorAll(".filer-button-expand");
+
+			expandButtons.forEach(function (button) {
+				button.addEventListener("click", function () {
+					const liElement = button.closest('li');
+					const ulElement = liElement.querySelector('ul');
+
+					if (ulElement) {
+						ulElement.classList.toggle("filter-display-on");
+						button.classList.toggle("filer-button-expand_active-expand");
+						const siblingUls = liElement.querySelectorAll('ul');
+						siblingUls.forEach(function (ul) {
+							if (ul !== ulElement) {
+								ul.classList.remove("filter-display-on");
+							}
+						});
+					}
+				});
+			});
 		}
 	});
 
-	const expandButtons = document.querySelectorAll(".filer-button-expand");
-
-	expandButtons.forEach(function (button) {
-		button.addEventListener("click", function () {
-			const liElement = button.closest('li');
-			const ulElement = liElement.querySelector('ul');
-
-			if (ulElement) {
-				ulElement.classList.toggle("filter-display-on");
-				button.classList.toggle("filer-button-expand_active-expand");
-				const siblingUls = liElement.querySelectorAll('ul');
-				siblingUls.forEach(function (ul) {
-					if (ul !== ulElement) {
-						ul.classList.remove("filter-display-on");
-					}
-				});
-			}
-		});
+	observer.observe(document.body, {
+		childList: true,
+		subtree: true
 	});
-
 });
